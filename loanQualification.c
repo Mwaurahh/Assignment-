911a9e0ea230419cc4440c28205ae2952d0d9c3b
Mwaurahh <mwaurasamson82@gmail.com>
2024-10-01 22:04:25 +0300

    Add closing curly brace loanQualification.c

--- loanQualification.c
+++ loanQualification.c
@@ -12,10 +12,11 @@ printf("Enter your monthly salary:");
 scanf("%lf",&salary);
 
 if (age >= 21 && salary >= 21000 ){
-    printf("Congratulations you qualify for a loan");
+printf("Congratulations you qualify for a loan");
 }
 else {
-    printf("Unfortunately, we are unable to offer you a loan at this time.");
+printf("Unfortunately, we are unable to offer you a loan at this time.");
 }
 return 0;
+}
 
