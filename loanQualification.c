e3d7cee7954726e732eaccb5d64102a72047159c
Mwaurahh <mwaurasamson82@gmail.com>
2024-10-01 22:14:34 +0300

    fix code indentation

--- loanQualification.c
+++ loanQualification.c
@@ -2,21 +2,20 @@
 
 int main (){ 
 
-int age ;
-double salary ;
-
-printf("Enter age:");
-scanf("%d",&age);
-
-printf("Enter your monthly salary:");
-scanf("%lf",&salary);
-
-if (age >= 21 && salary >= 21000 ){
-printf("Congratulations you qualify for a loan");
+  int age;
+  double salary;
+  
+  printf("Enter age:");
+  scanf("%d",&age);
+  
+  printf("Enter your monthly salary:");
+  scanf("%lf",&salary);
+  
+  if (age >= 21 && salary >= 21000 ){
+    printf("Congratulations you qualify for a loan");
+  }
+  else {
+    printf("Unfortunately, we are unable to offer you a loan at this time.");
+  }
+  return 0;
 }
-else {
-printf("Unfortunately, we are unable to offer you a loan at this time.");
-}
-return 0;
-}
-
