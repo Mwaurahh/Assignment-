5e62bec3df1ca8098c8ed0946f24c7ed162e8e2f
Mwaurahh <mwaurasamson82@gmail.com>
2024-10-01 22:14:24 +0300

    Create simpleInterest.c
    A c program to calculate simple interest

---
+++ simpleInterest.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+
+  int main()
+{
+   
+    double principle ;
+    double time ;
+    double rate ;
+    
+    printf("Enter principle:");
+    scanf("%lf", &principle);
+    
+    printf("Enter time :");
+    scanf("%lf",&time);
+    
+    printf("Enter the rate:");
+    scanf("%lf",&rate);
+    
+     double simpleInterest ;
+   
+    simpleInterest = (principle*time*rate)/100;
+    printf("The simpleIntrest is %.2lf", simpleInterest);
+   
+    return 0;
+}
